3006f9d1c2bd37aee68082cc370c9be12694c4bb
Alvaro <astaboada7@gmail.com>
2021-02-08 10:05:57 +0100

    first commit

---
+++ database/factories/DishFactory.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Dish;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class DishFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Dish::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'name' => $this->faker->name,
+            'image' => $this->faker->image,
+            'description' => $this->faker->sentence(8),
+        ];
+    }
+}
